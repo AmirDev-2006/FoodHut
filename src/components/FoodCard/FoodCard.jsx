6fb085c75ac222ed56c17f089a137a7c222cb90f
Amir Hossein Kheirollahpour <amir1.kh2006@gmail.com>
2025-02-24 01:22:10 +0330

    card

---
+++ src/components/FoodCard/FoodCard.jsx
@@ -0,0 +1,28 @@
+import KebabFood from '../../assets/images/KebabFood.png'
+export default function FoodCard() {
+  return (
+    <>
+      <div className="flex mt-5 flex-col relative rounded-[20px] gradiant2 bg-gradient-to-t from-[#ed9797] to-white  items-center w-[310px] h-[368px]">
+        <div className="w-[248px] flex items-center justify-center absolute bottom-52 h-[248px] border-4 bg-gradient-to-b from-[#f5bc8b] to-[white] rounded-full">
+          <img
+            className="w-[198px] h-[198px] border-white border-[10px] rounded-full"
+            src={KebabFood}
+            alt=""
+          />
+        </div>
+        <div className="w-[243px] h-[63px] absolute bottom-32 text-center flex flex-col gap-y-2">
+          <h1 className="text-[#f54748] text-2xl font-sofia-bold tracking-wide">
+            Kebab
+          </h1>
+          <p className="text-[16px] tracking-tight">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry
+          </p>
+        </div>
+        <button className="absolute -bottom-4 w-[131px] h-[45px] bg-[#f54748] rounded-[100px] text-white">
+          Order Now
+        </button>
+      </div>
+    </>
+  );
+}
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,6 +2,7 @@ import kebab from '../../assets/images/kebab.png'
 import ChickenTikka from '../../assets/images/Chicken Tikka.png'
 import ChickenChargha from '../../assets/images/Chicken Chargha.png'
 import DesiChowmein from '../../assets/images/Desi Chowmein.png'
+import FoodCard from '../FoodCard/FoodCard'
 export default function Menu() {
   return (
     <>
@@ -21,19 +22,33 @@ export default function Menu() {
           <button className="res2">Desserts</button>
           <button className="res2">Drinks</button>
         </div>
-        <div className="flex justify-center flex-wrap w-full ">
-            <div className='flex gap-x-[18px]'>
-                <img src={kebab} alt="" />
-                <img className='res4' src={ChickenTikka} alt="" />
-                <img className='res2' src={ChickenChargha} alt="" />
-                <img className='res2 hidden xl:flex ' src={DesiChowmein} alt="" />
-            </div>
-            <div className='flex gap-x-[18px] mt-[66px] res2'>
-                <img src={kebab} alt="" />
-                <img src={ChickenTikka} alt="" />
-                <img src={ChickenChargha} alt="" />
-                <img className='hidden xl:flex' src={DesiChowmein} alt="" />
-            </div>
+        <div className="flex justify-center flex-wrap w-full mb-9 ">
+           <div className="flex flex-row flex-wrap gap-y-28 mt-10 justify-center gap-x-[18px]">
+                     <div className="flex gap-x-4 gap-y-20">
+                       <FoodCard/>
+                       <div className="res4">
+                       <FoodCard/>
+                       </div>
+                       <div className="res2" >
+                       <FoodCard/>
+                       </div>
+                       <div className="res2 hidden xl:flex">
+                       <FoodCard/>
+                       </div>
+                     </div>
+                     <div className="flex gap-x-4 res2 gap-y-20 ">
+                       <FoodCard/>
+                       <div className="res4">
+                       <FoodCard/>
+                       </div>
+                       <div className="res2" >
+                       <FoodCard/>
+                       </div>
+                       <div className="res2 hidden xl:flex">
+                       <FoodCard/>
+                       </div>
+                     </div>
+                   </div>
         </div>
       </div>
     </>
